4fd4b328f3280d66949af780ce68f8161b872a77
Mike Ayers <mike@vesiron.com>
2026-02-06 22:37:05 +0000

    Fix SW build

--- quickcalc/sw.js
+++ quickcalc/sw.js
@@ -5,21 +5,34 @@ const urlsToCache = [
   '/quickcalc/manifest.json'
 ];
 
+// Install service worker and cache assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
   self.skipWaiting();
 });
 
+// Fetch from cache, fallback to network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
 
+// Clean up old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
